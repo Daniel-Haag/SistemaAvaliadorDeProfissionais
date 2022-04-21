7e16ca29e6f248596e17ca560add8db265787618
Daniel Haag <danni.haag91@gmail.com>
2022-04-21 10:39:14 -0300

    aaa

--- SistemaAvaliacaoDeProfissionais/ClientApp/src/app/components/evaluation/evaluation.component.ts
+++ SistemaAvaliacaoDeProfissionais/ClientApp/src/app/components/evaluation/evaluation.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+//import { ResultadoAvaliacao } from '../../models/resultadoAvaliacao.model';
 
 @Component({
   selector: 'app-evaluation',
@@ -8,13 +10,29 @@ import { Component, OnInit } from '@angular/core';
 export class EvaluationComponent implements OnInit {
 
   //Aqui deve buscar os questionamentos (operacional, tático, estratégico)
-  questions: string[] = ["Questão 1", "Questão 2", "Questão 3"];
+  questions: string[] = ["Questao1", "Questao2", "Questao3", "Questao4", "Questao5"];
   options: string[] = ["1", "2", "3", "4", "5"];
+
+  form = new FormGroup({
+    Questao1: new FormControl('', Validators.required),
+    Questao2: new FormControl('', Validators.required),
+    Questao3: new FormControl('', Validators.required),
+    Questao4: new FormControl('', Validators.required),
+    Questao5: new FormControl('', Validators.required)
+  });
+
+  get f() {
+    return this.form.controls;
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
+
 
   constructor() { }
 
   ngOnInit(): void {
-    
-  }
 
+  }
 }
